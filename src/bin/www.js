b69a0a588aea70e873899171fbe9b3ff61f6467c
zcason <zuri.cason@gmail.com>
2024-01-28 13:58:34 -0600

    added security middleware

--- src/bin/www.js
+++ src/bin/www.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
 import { migrateLatest, verifyConnection } from '@Database';
 import { logger, PORT } from '@Utils';
 import rootRouter, { handleErrors } from '@Routes'; 
@@ -6,18 +8,29 @@ import gracefulShutdown from './gracefulShutdown';
 
 
 const app = express();
+// reevalute cors before depolying
+const corsOption = {
+    origin: ['http://localhost:3000'],
+};
 
 async function initialize() {
     logger.info('initializing service...');
-
-    // hide server type from browser
-    app.disable('x-powered-by');
 
     // verify connection to postgres
     await verifyConnection();
 
     // run migrations
     await migrateLatest();
+
+    // Make sure the 'x-powered-by' is disabled by default
+    app.use(helmet());
+
+    // hide server type from browser 
+    // app.disable('x-powered-by'); 
+
+    app.use(cors(corsOption));
+
+    app.use(express.json());
 
     // set routes
     app.use('/api', rootRouter);
